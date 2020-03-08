18d5daf0d5239f8a896ab9a721850ee8ece8b4a1
roseline124 <guseod24@gmail.com>
2020-03-08 15:00:11 +0900

    feature: add Counter (useState)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import MyComponent from './components/MyComponent'
+import Counter from './components/Counter'
 
 function App() {
-	return (
-		<MyComponent name="First Function Component" favoriteNumber={4}>
-			<p style={{ color: 'red' }}>children</p>
-		</MyComponent>
-	)
+	return <Counter />
 }
 
 export default App
---
+++ src/components/Counter.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+	const [count, setCount] = useState(0)
+
+	const handleClickPlus = () => {
+		const newCount = count + 1
+		setCount(newCount)
+	}
+
+	const handleClickMinus = () => {
+		const newCount = count - 1
+		setCount(newCount)
+	}
+
+	return (
+		<div>
+			current Number is {count}
+			<div>
+				<button onClick={handleClickPlus}>+1</button>
+				<button onClick={handleClickMinus}>-1</button>
+			</div>
+		</div>
+	)
+}
+
+export default Counter
