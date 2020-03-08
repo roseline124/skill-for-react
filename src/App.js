7ff067c72f3e822988bcafff7e1f83c88709b166
roseline124 <guseod24@gmail.com>
2020-03-08 14:44:17 +0900

    Add props to function component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react'
 import MyComponent from './components/MyComponent'
 
 function App() {
-	return <MyComponent />
+	return (
+		<MyComponent name="First Function Component" favoriteNumber={4}>
+			<p style={{ color: 'red' }}>children</p>
+		</MyComponent>
+	)
 }
 
 export default App
--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const MyComponent = () => {
-	return <div>My Component</div>
+const MyComponent = ({ name, children, favoriteNumber }) => {
+	return (
+		<div>
+			My Component Name: {name} <br />
+			Children: {children} <br />
+			Favorite Number: {favoriteNumber}
+		</div>
+	)
+}
+
+MyComponent.defaultProps = {
+	name: 'default name'
+}
+
+MyComponent.propTypes = {
+	name: PropTypes.string,
+	children: PropTypes.node,
+	favoriteNumber: PropTypes.number.isRequired
 }
 
 export default MyComponent
