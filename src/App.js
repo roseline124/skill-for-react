2e52946817b865715567a990e0a28590a17e0dce
roseline124 <guseod24@gmail.com>
2020-03-15 00:28:22 +0900

    Use router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Home from 'components/Home'
 
 function App() {
-  return <div>App</div>
+  return (
+    <Router>
+      <Route path="/" component={Home} exact />
+    </Router>
+  )
 }
 
 export default App
